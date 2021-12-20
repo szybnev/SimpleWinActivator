d0922d299525f37e325713d60f2acad00dc9135f
Zybnev Sergey <32132485+szybnev@users.noreply.github.com>
2021-12-20 12:41:49 +0300

    Add files via upload

---
+++ SimpleWinActivator.bat
@@ -0,0 +1,61 @@
+@echo off
+
+echo Hello! It's simple activation tool
+echo Please wait...
+IF "%1%"=="1" (
+	slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="2" (
+	slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="3" (
+	slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="4" (
+	slmgr /ipk PVMJN-6DFY6–9CCP6–7BKTT-D3WVR
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="5" (
+	slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="6" (
+	slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="7" (
+	slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="8" (
+	slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="9" (
+	slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+IF "%1%"=="10" (
+	slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+	slmgr /skms kms8.msguides.com
+	slmgr /ato
+	)
+
+echo Error! Invalid argument.
+goto Exit
+
+echo https://github.com/szybnev
+
+pause
